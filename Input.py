9521b985eaf0676f517ac176febe3ca1b692352a
Bhagyashree Chavan <bhagyashree.chavan90@gmail.com>
2020-04-24 15:55:54 +0530

    SPRINT 3: Handling Program Flow in Python
    Input 2D array

--- Input.py
+++ Input.py
@@ -1,3 +1,11 @@
 print("Enter name:")
 name= input()
 print("Welcome "+name)
+if False:
+    m , n  = input().split()
+    m , n  = int(m) , int(n)
+    my_array = []
+    for i in range(int(m)):
+        row = [ int(x) for x in input().split()]
+        my_array.append(row)
+    print(my_array)
