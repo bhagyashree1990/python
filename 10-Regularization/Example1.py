a77c9870bb6c83b1caa56755d7eaa0ecf1165aa0
Bhagyashree Chavan <bhagyashree.chavan@sts.in>
2020-05-28 14:07:20 +0530

    SPRINT 11: Regularization
    Polynomial Regressor

--- 10-Regularization/Example1.py
+++ 10-Regularization/Example1.py
@@ -9,8 +9,6 @@ df = pd.read_csv(path)
 X = df.loc[:,df.columns != 'list_price']
 y = df['list_price']
 
-
-
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3, random_state=6)
 
 # Add square and cubic powers of 'GarageScore' to train and test
--- 10-Regularization/Moving to Melbourne/Example6.py
+++ 10-Regularization/Moving to Melbourne/Example6.py
@@ -1,1 +1,12 @@
 #Prediction Using Polynomial Regressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from Example1 import X_train, X_test, y_train, y_test
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+#Code starts here
+model = make_pipeline(PolynomialFeatures(2), LinearRegression())
+model.fit(X_train,y_train)
+y_pred = model.predict(X_test)
+r2_poly = r2_score(y_test,y_pred)
+print(r2_poly)
